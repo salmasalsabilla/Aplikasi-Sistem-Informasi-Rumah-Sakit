a46d2be7454c0c304bc1c1d879167de15dee174e
Salma Salsabila <62732034+salmasalsabilla@users.noreply.github.com>
2022-05-09 13:53:51 +0700

    Add files via upload

---
+++ RumahSakitFinal/src/rumahsakit/ProjectRumahSakit.java
@@ -0,0 +1,11 @@
+package rumahsakit;
+
+public class ProjectRumahSakit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new menuUtama().setVisible(true);
+    }
+}
